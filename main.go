42829dfd341f71f9ba098d9b2f5f0e24c7674a4b
wesleynepo <wesleynepomuceno1@gmail.com>
2022-10-29 23:53:25 -0300

    feat: token generation

---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pascaldekloe/jwt"
+)
+
+func main() {
+    r := gin.Default()
+
+    r.GET("/ping", func (c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "pong",
+        })
+    })
+
+    r.POST("/signup", func (c *gin.Context) {
+        //TODO: Sign up
+    })
+
+    r.POST("/login", func (c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "token": generateToken(),
+            "refresh": generateRefreshToken(),
+        })
+    })
+
+    r.POST("/refresh", func (c *gin.Context) {
+        body := RefreshRequest{}
+
+        if err := c.BindJSON(&body); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "aqui": "debug"})
+			return
+        }
+
+        response, err := refreshToken(body)
+
+        if (err != nil) {
+            c.JSON(http.StatusBadRequest, gin.H{"error1": err.Error()})
+			return
+        }
+
+        c.JSON(http.StatusOK, response)
+    })
+
+    r.POST("/confirmation/:code", func (c *gin.Context) {
+        code := c.Param("code")
+        c.JSON(http.StatusOK, gin.H{
+            "code": code,
+        })
+        //TODO: Confirmation link
+    })
+
+    r.Run()
+}
+
+func refreshToken(request RefreshRequest) (*RefreshResponse, error) {
+    claimsRefresh, err := jwt.HMACCheck([]byte(request.Refresh), []byte("test"))
+
+    if (err != nil) {
+        return nil, err
+    }
+
+    claimsToken, err := jwt.HMACCheck([]byte(request.Token), []byte("test"))
+
+    if (err != nil) {
+        return nil, err
+    }
+
+    if (claimsRefresh.Valid(time.Time{})) {
+        return nil, errors.New("Refresh token expired")
+    }
+
+    token, err := createToken(claimsToken.Set["id"].(float64))
+
+    if (err != nil) {
+        return nil, err
+    }
+
+    return createRefreshResponse(token, request.Refresh), nil
+}
+
+func createRefreshResponse(token, refreshToken string) *RefreshResponse {
+    return &RefreshResponse{Token: token, Refresh: refreshToken}
+}
+
+func createToken(userId float64) (string, error) {
+    var claims jwt.Claims
+
+    claims.Set = map[string]interface{}{"id": userId}
+    claims.Issued  = jwt.NewNumericTime(time.Now().Round(time.Second))
+    claims.Expires = jwt.NewNumericTime(time.Now().Add(time.Hour * 4))
+
+    token, err := claims.HMACSign("HS256", []byte("test"))
+
+    if (err != nil) {
+        return "", err
+    }
+
+    return string(token), nil
+}
+
+func generateToken() (string) {
+    token, err := createToken(123)
+
+    if (err != nil) {
+        log.Printf("error ocurred %v", err)
+    }
+
+    return token
+}
+
+func generateRefreshToken() (string) {
+    var claims jwt.Claims
+
+    claims.Set = map[string]interface{}{"id": 123}
+    claims.Issued  = jwt.NewNumericTime(time.Now().Round(time.Second))
+    claims.Expires = jwt.NewNumericTime(time.Now().Add(time.Hour * 24))
+
+    token, err := claims.HMACSign("HS256", []byte("test"))
+
+    if (err != nil) {
+        log.Printf("error ocurred %v", err)
+    }
+
+    return string(token)
+}
+
+type RefreshRequest struct {
+    Refresh string `json:"refresh"`
+    Token string `json:"token"`
+}
+
+type RefreshResponse = RefreshRequest
